f33706c81a1a9bdd696117f54370a0f4e3f047bf
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-15 16:14:39 -0500

    Modify 'Header.jsx' component containing 'Resume' and 'HeadSocial' components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import HeaderSocial from './HeaderSocial'
+import Resume from './Resume'
+import ME from '../../assets/me.png'
+import './header.css'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Robin Benitez</h1>
+        <h5 className='text-light'>Full Stack Developer</h5>
+        <Resume />
+        <HeaderSocial />
+
+        <div>
+          <img src={ME} alt='me' className='me' />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
