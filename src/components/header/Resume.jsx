62e54147d1a468679b01c38658361f5964a405de
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-15 15:59:12 -0500

    Add 'Resume' and 'HeaderSocial' components for the 'Head' component

---
+++ src/components/header/Resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CV from '../assets/cv.pdf'; // Import pdf file from assets folder
+
+const Resume = () => {
+  return (
+    <div className='resume'>
+      <a href={CV} download className='btn'>Download Resume</a>
+      <a href="#contact" className='btn-primary-class'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default Resume
