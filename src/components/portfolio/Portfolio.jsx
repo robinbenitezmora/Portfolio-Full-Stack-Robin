067f031648ddff41bee543ecb5db60712b98c670
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-16 17:35:46 -0500

    Add logic to 'Portfolio.jsx' component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Works</h5>
+      <h2>My Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='project1' />
+          </div>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className='btn'>GitHub</a>
+          <a href="https://dribbble.com/Allien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
