ceef43e7470a765794587fc8b73fc38d08f0785d
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-15 21:10:13 -0500

    Modify 'About.jsx' component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='My image' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__card-icon' />
+              <h5>Experience</h5>
+              <small>1+ Year Working as a Developer</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__card-icon' />
+              <h5>Projects</h5>
+              <small>50+ Designed</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__card-icon' />
+              <h5>Happy Clients</h5>
+              <small>50+ Satisfied</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi iste id tempora in ex aspernatur sed asperiores. Dolorum aliquid autem quisquam labore, laudantium consequuntur similique amet consectetur debitis neque. Esse.</p>
+          <a href="#contact" className='btn btn-primary'>Let's to Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
