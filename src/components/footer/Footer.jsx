b0f5ee1586acf0f2afee709b9c5343d142b07d9f
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-17 20:00:59 -0500

    Add logic to 'Footer.jsx' component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>ROBIN</a>
+
+      <ul className='list'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#service">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimmonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://www.facebook.com/robinbenitez" target="_blank"><FaFacebook /></a>
+        <a href="https://twitter.com/robinbenitez" target="_blank"><FiInstagram /></a>
+        <a href="https://twitter.com/robinbenitez" target="_blank"><IoLogoTwitter /></a>
+      </div>
+
+      <div className='footer__notes'>
+        <small>&copy; ROBIN Works. All right reserved</small>        
+      </div>
+    </footer>
   )
 }
 
